dc984710c623f2751a91ec05db94c00d761638f1
alexngumbau <111250066+alexngumbau@users.noreply.github.com>
2023-04-20 16:35:26 +0300

    Delete day 3 directory

--- day 3/SQLQuery_3.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-
-    EnglishProductName AS ProductName,
-    EnglishDescription AS ProductDescription,
-    CONCAT(EnglishProductName,'-',EnglishDescription) AS ProductNameDescription
-
-FROM DimProduct
-
-WHERE ProductKey = 555
