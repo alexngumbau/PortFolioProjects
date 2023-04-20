35bedc4f3fe3859aadf62eacb76aa5d7c8cd1193
alexngumbau <111250066+alexngumbau@users.noreply.github.com>
2023-04-20 16:35:40 +0300

    Delete Day 4 directory

--- Day 4/SQLQuery_2.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-FirstName,
-EmailAddress
-
-FROM DimCustomer
-
-WHERE FirstName LIKE 'R%'
---WHERE FirstName LIKE '__R%' --SELECTS A NAME WHERE R IS THE 3RD LETTER
---WHERE FirstName LIKE '_R%' --SELECTS A NAME WHERE R IS THE SECOND LETTER
--- Day 4/SQLQuery_3.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-
-    FirstName,
-    LastName,
-    YearlyIncome,
-    EmailAddress,
-    IIF(YearlyIncome > 50000, 'Above Average', 'Below Average') AS IncomeCategory
-
-FROM DimCustomer
